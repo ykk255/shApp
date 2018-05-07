9b7b554dcaee2edcfbfbb74752a5297aed7bd106
seohee <ykk255@naver.com>
2018-05-08 02:39:46 +0900

    화면에러+화면이동X

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
-
+import * as $ from 'jquery';
+window["$"] = $;
+window["jQuery"] = $;
 
 @Component({
   selector: 'app-root',
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+//import { Chart } from 'chart.js/dist/Chart.min.js';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
+
 
 if (environment.production) {
   enableProdMode();
