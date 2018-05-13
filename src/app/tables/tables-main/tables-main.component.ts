8a29647c0667675378819cb515427817f0e1d9a2
seohee <ykk255@naver.com>
2018-05-13 15:48:27 +0900

    ㅇㅇㅇ

--- src/app/tables/tables-main/tables-main.component.ts
+++ src/app/tables/tables-main/tables-main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ArrayType } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-tables-main',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tables-main.component.css']
 })
 export class TablesMainComponent implements OnInit {
+  tableList = [{"nm" : "kim", "age": 27, "city" : "seoul"},
+  {"nm" : "lee", "age": 24, "city" : "busan"},
+  {"nm" : "park", "age": 24, "city" : "newyork"}];
+
+  arr = new Array<any>();
+  map = new Map<string, string>();
 
   constructor() { }
 
   ngOnInit() {
+    this.map;
   }
 
+  filterTest (arr: any): Array<any> {
+    return arr.filter(ar => ar.age == 24);
+  }
 }
